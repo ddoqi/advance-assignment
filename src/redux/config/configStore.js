b375c841d5ea44931c35690a8c1b818a1a6d129f
hongdakyung <bongbk72@naver.com>
2023-01-09 04:12:13 +0900

    fix : add logic when input data
    - redux-persist를 사용하여 새로고침시 데이터가 리셋되지 않도록 수정하였습니다
    
    by ddoqi

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -1,8 +1,22 @@
-import { configureStore } from "@reduxjs/toolkit";
-import todos from "../modules/todos";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
+import { persistReducer } from "redux-persist";
+import { todosSlice } from "../modules/todos";
+
+const reducers = combineReducers({
+  todos: todosSlice.reducer,
+});
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ['todos'],
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
 
 const store = configureStore({
-  reducer: { todos },
+  reducer: persistedReducer,
 });
 
 export default store;
